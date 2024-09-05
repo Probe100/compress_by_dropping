64450b426821783447ad634d6a82632abc86691a
Probe <1026657295@qq.com>
2024-09-05 14:52:44 +0800

    delete fintune code

--- experiments/qwen2_decode_layer_drop.sh
+++ experiments/qwen2_decode_layer_drop.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=2 python run_decode_layer_drop.py --n_drop=3 --cache_filena
 CUDA_VISIBLE_DEVICES=3 python run_decode_layer_drop.py --n_drop=4 --cache_filename Qwen2_7B_similarity_BD_4.pt --model_save_path models/Qwen2-7B-BD4 & 
 CUDA_VISIBLE_DEVICES=4 python run_decode_layer_drop.py --n_drop=5 --cache_filename Qwen2_7B_similarity_BD_5.pt --model_save_path models/Qwen2-7B-BD5 & 
 CUDA_VISIBLE_DEVICES=5 python run_decode_layer_drop.py --n_drop=6 --cache_filename Qwen2_7B_similarity_BD_6.pt --model_save_path models/Qwen2-7B-BD6 & 
-#CUDA_VISIBLE_DEVICES=6 python run_decode_layer_drop.py --n_drop=7 --cache_filename Qwen2_7B_similarity_BD_7.pt --model_save_path models/Qwen2-7B-BD7 & 
-#CUDA_VISIBLE_DEVICES=7 python run_decode_layer_drop.py --n_drop=8 --cache_filename Qwen2_7B_similarity_BD_8.pt --model_save_path models/Qwen2-7B-BD8 &
+CUDA_VISIBLE_DEVICES=6 python run_decode_layer_drop.py --n_drop=7 --cache_filename Qwen2_7B_similarity_BD_7.pt --model_save_path models/Qwen2-7B-BD7 & 
+CUDA_VISIBLE_DEVICES=7 python run_decode_layer_drop.py --n_drop=8 --cache_filename Qwen2_7B_similarity_BD_8.pt --model_save_path models/Qwen2-7B-BD8 &
